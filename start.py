df5897abcab5c016fd8b140b2810f1b2301264d8
Yan Gabriel <yangabrielogy@gmail.com>
2022-03-13 02:48:25 -0300

    PEP8 changes and a smarter code! (:

--- start.py
+++ start.py
@@ -9,14 +9,7 @@ def askQuestion(correct: str, word: str, tips, count=0, wins=0):
     anwser = input(f'Que palavra é essa? {word} ')
 
     if anwser.upper() == correct.upper():  # When the user answers correctly.
-        wins += 1
-
-        os.system('cls')
-        print(f'Você conseguiu! A palavra era {correct}.')
-        print(f'Vamos tentar outra palavra? Você conseguiu {wins} vitorias.')
-
-        time.sleep(3)
-        startGame(count, wins)
+        correctAnwser(wins, correct, count)
 
     elif anwser.upper() == 'AGAIN':  # Restart the game with other word.
         print(f'Você desistiu com {count} tentativas e {wins} acertos...')
@@ -26,23 +19,18 @@ def askQuestion(correct: str, word: str, tips, count=0, wins=0):
         return startGame()
 
     elif anwser.upper() == 'TIPS':
+        os.system('cls')
+
         if tips:
             if len(tips) > 1:
-                os.system('cls')
                 print('Você tem dicas! Vamos revelar apenas uma por vez.')
-                print(tips.pop(0))
-                askQuestion(correct, word, tips, count, wins)
-
             else:
-                os.system('cls')
                 print('Você tem uma dica! Vamos revelar ela pra você.')
-                print(tips.pop(0))
-                askQuestion(correct, word, tips, count, wins)
-
+            print(tips.pop(0))
         else:
-            os.system('cls')
             print('Você está sem dicas no momento.')
-            askQuestion(correct, word, tips, count, wins)
+
+        askQuestion(correct, word, tips, count, wins)
 
     elif anwser.upper() == 'EXIT':
         print(f'Você estava em uma sequência de {wins} acertos.')
@@ -59,6 +47,17 @@ def askQuestion(correct: str, word: str, tips, count=0, wins=0):
         os.system('cls')
         print(f'Errado, tente de novo! (Tentativas: {count})')
         askQuestion(correct, word, tips, count, wins)
+
+
+def correctAnwser(wins, correct, count):
+    wins += 1
+
+    os.system('cls')
+    print(f'Você conseguiu! A palavra era {correct}.')
+    print(f'Vamos tentar outra palavra? Você conseguiu {wins} vitorias.')
+
+    time.sleep(3)
+    startGame(count, wins)
 
 
 def startGame(count=0, wins=0):
